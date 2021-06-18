f599243936ebddc367c3b1c951908f1dea394aca
danibarker <caseybarker86@gmail.com>
2021-06-18 09:54:10 -0600

    deleted js files

---
+++ main.py
@@ -0,0 +1,38 @@
+# def new_function(arg1,arg2):
+#     print('hello')
+
+# # new_function(1,2)
+# # name = 'DaniB'
+# # if name=='Dani':
+# #     print('Dani')
+# # elif name=='DaniB':
+# #     print('DaniB')
+# # else:
+# #    pass
+
+# # nums = [1,2,3,4]
+# # for num in nums:
+# #     print(num)
+
+# for num in range(6):
+#     print(num)
+
+# import second_file
+# from second_file import hello
+# import second_file as sf
+
+# sf.hello()
+# hello()
+
+
+from second_file import n
+
+print(n)
+
+# while n<5:
+#     print(n)
+
+import packages.package1
+
+
+print(f"n is {n}")
